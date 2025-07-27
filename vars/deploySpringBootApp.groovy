a09e2f42e10662103533f0bf51c7aca398297f81
Rakesh Singh <rrrbisht91@gmail.com>
2025-07-27 13:02:26 +0530

    Update deploySpringBootApp.groovy
    added the credentail part for the git commit

--- vars/deploySpringBootApp.groovy
+++ vars/deploySpringBootApp.groovy
@@ -83,14 +83,16 @@ def call(Map config) {
                             sed -i 's|sinra12/springboot-myfirstdocker:latest|${imageTag}|g' k8s/k8s-deployment.yaml
                         """
                         
-                        // Commit and push the changes to the Git repository
-                        sh """
-                            git config --global user.name "jenkins"
-                            git config --global user.email "jenkins@example.com"
-                            git add k8s/k8s-deployment.yaml
-                            git commit -m "Updated Docker image tag to ${imageTag}"
-                            git push origin HEAD:main
-                        """
+                         // Commit and push the changes to the Git repository using Jenkins credentials
+                    withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
+                       sh """
+                        git config --global user.name "jenkins"
+                        git config --global user.email "jenkins@example.com"
+                        git add k8s/k8s-deployment.yaml
+                        git commit -m "Updated Docker image tag to ${imageTag}"
+                        git push https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/your-username/your-repo.git HEAD:main
+                    """
+                       }
                     }
                 }
             }
